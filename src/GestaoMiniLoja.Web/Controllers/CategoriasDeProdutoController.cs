55aa23b81ed6b9b5a783117968626ecd4793b5c3
Marcio <marcio28@gmail.com>
2025-03-30 22:15:42 -0300

    Implementando CRUD de Produto.

--- src/GestaoMiniLoja.Web/Controllers/CategoriasDeProdutoController.cs
+++ src/GestaoMiniLoja.Web/Controllers/CategoriasDeProdutoController.cs
@@ -8,15 +8,13 @@ namespace GestaoMiniLoja.Web.Controllers
     [Route("categorias-de-produto")]
     public class CategoriasDeProdutoController(ApplicationDbContext context) : Controller
     {
-        private const string AccessNotConfiguredMessage = "Não configurado o acesso às categorias de produto.";
+        private const string MensagemAcessoNaoConfigurado = "Não configurado o acesso aos dados de categorias de produto.";
         private readonly ApplicationDbContext _context = context;
 
         public async Task<IActionResult> Index()
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
             return View(await _context.CategoriasDeProduto.ToListAsync());
         }
@@ -25,42 +23,34 @@ namespace GestaoMiniLoja.Web.Controllers
         public async Task<IActionResult> Details(int id)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
-            var categoriaDeProduto = await _context.CategoriasDeProduto
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var categoriaDeProduto = await _context.CategoriasDeProduto.FirstOrDefaultAsync(m => m.Id == id);
+
             if (categoriaDeProduto == null)
-            {
                 return NotFound();
-            }
 
             return View(categoriaDeProduto);
         }
 
         [Route("nova")]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
+        public IActionResult Create() => View();
    
         [HttpPost("nova")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Descricao")] CategoriaDeProduto categoriaDeProduto)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
             if (ModelState.IsValid)
             {
                 _context.Add(categoriaDeProduto);
                 await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(categoriaDeProduto);
         }
 
@@ -68,15 +58,13 @@ namespace GestaoMiniLoja.Web.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
             var categoriaDeProduto = await _context.CategoriasDeProduto.FindAsync(id);
+
             if (categoriaDeProduto == null)
-            {
                 return NotFound();
-            }
+
             return View(categoriaDeProduto);
         }
 
@@ -85,14 +73,10 @@ namespace GestaoMiniLoja.Web.Controllers
         public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao")] CategoriaDeProduto categoriaDeProduto)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
             if (id != categoriaDeProduto.Id)
-            {
-                return NotFound();
-            }
+                return BadRequest();
 
             if (ModelState.IsValid)
             {
@@ -104,16 +88,16 @@ namespace GestaoMiniLoja.Web.Controllers
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!CategoriaDeProdutoExists(categoriaDeProduto.Id))
-                    {
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
+
+                TempData["Sucesso"] = "Categoria de produto editada com sucesso.";
+
                 return RedirectToAction(nameof(Index));
             }
+
             return View(categoriaDeProduto);
         }
 
@@ -121,16 +105,12 @@ namespace GestaoMiniLoja.Web.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
-            var categoriaDeProduto = await _context.CategoriasDeProduto
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var categoriaDeProduto = await _context.CategoriasDeProduto.FirstOrDefaultAsync(m => m.Id == id);
+
             if (categoriaDeProduto == null)
-            {
                 return NotFound();
-            }
 
             return View(categoriaDeProduto);
         }
@@ -141,23 +121,19 @@ namespace GestaoMiniLoja.Web.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             if (_context.CategoriasDeProduto == null)
-            {
-                return Problem(AccessNotConfiguredMessage);
-            }
+                return Problem(MensagemAcessoNaoConfigurado);
 
             var categoriaDeProduto = await _context.CategoriasDeProduto.FindAsync(id);
-            if (categoriaDeProduto != null)
-            {
-                _context.CategoriasDeProduto.Remove(categoriaDeProduto);
-            }
 
+            if (categoriaDeProduto == null)
+                return NotFound();
+
+            _context.CategoriasDeProduto.Remove(categoriaDeProduto);
             await _context.SaveChangesAsync();
+
             return RedirectToAction(nameof(Index));
         }
 
-        private bool CategoriaDeProdutoExists(int id)
-        {
-            return _context.CategoriasDeProduto.Any(e => e.Id == id);
-        }
+        private bool CategoriaDeProdutoExists(int id) => _context.CategoriasDeProduto.Any(e => e.Id == id);
     }
 }
---
+++ src/GestaoMiniLoja.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,192 @@
+﻿using GestaoMiniLoja.Data.Data;
+using GestaoMiniLoja.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoMiniLoja.Web.Controllers
+{
+    public class ProdutosController(ApplicationDbContext context, UserManager<IdentityUser> userManager) : Controller
+    {
+        const string MensagemAcessoNaoConfigurado = "Não configurado o acesso aos dados de produtos.";
+        const string MensagemUsuarioNaoIdentificado = "Usuário não identificado.";
+        readonly ApplicationDbContext _context = context;
+        readonly UserManager<IdentityUser> _userManager = userManager;
+        string? _usuarioIdString;
+
+        [Route("produtos")]
+        public async Task<IActionResult> Index()
+        {
+            _usuarioIdString = await ObterUsuarioIdAsync();
+
+            if (_usuarioIdString == null) 
+                return Problem(MensagemUsuarioNaoIdentificado);
+
+            var applicationDbContext = _context.Produtos.Include(p => p.CategoriaDeProduto)
+                                                        .Include(p => p.Vendedor)
+                                                        .Where(p => p.VendedorId.ToString() == _usuarioIdString);
+
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        [Route("detalhes/{id:int}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            if (_context.Produtos == null) 
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            _usuarioIdString = await ObterUsuarioIdAsync();
+
+            if (_usuarioIdString == null) 
+                return Problem(MensagemUsuarioNaoIdentificado);
+
+            var produto = await _context.Produtos.Include(p => p.CategoriaDeProduto)
+                                                 .Include(p => p.Vendedor)
+                                                 .FirstOrDefaultAsync(m => m.Id == id);
+            
+            if (produto == null) 
+                return NotFound();
+
+            if (produto.VendedorId.ToString() != _usuarioIdString) return BadRequest();
+
+            return View(produto);
+        }
+
+        [Route("novo")]
+        public IActionResult Create()
+        {
+            ViewData["CategoriaDeProdutoId"] = new SelectList(_context.CategoriasDeProduto, "Id", "Descricao");
+            //ViewData["VendedorId"] = new SelectList(_context.Vendedores, "Id", "Id");
+
+            return View();
+        }
+
+        [HttpPost("novo")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao,CaminhoDaImagem,PrecoUnitario,QuantidadeEmEstoque,CategoriaDeProdutoId,VendedorId")] Produto produto)
+        {
+            if (_context.Produtos == null) 
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            _usuarioIdString = await ObterUsuarioIdAsync();
+
+            if (_usuarioIdString == null) 
+                return Problem(MensagemUsuarioNaoIdentificado);
+
+            if (ModelState.IsValid)
+            {
+                produto.VendedorId = new Guid(_usuarioIdString);
+
+                _context.Add(produto);
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoriaDeProdutoId"] = new SelectList(_context.CategoriasDeProduto, "Id", "Descricao", produto.CategoriaDeProdutoId);
+            //ViewData["VendedorId"] = new SelectList(_context.Vendedores, "Id", "Id", produto.VendedorId);
+
+            return View(produto);
+        }
+
+        [Route("editar/{id:int}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (_context.Produtos == null) 
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+                return NotFound();
+
+            ViewData["CategoriaDeProdutoId"] = new SelectList(_context.CategoriasDeProduto, "Id", "Descricao", produto.CategoriaDeProdutoId);
+            //ViewData["VendedorId"] = new SelectList(_context.Vendedores, "Id", "Id", produto.VendedorId);
+
+            return View(produto);
+        }
+
+        [HttpPost("editar/{id:int}"), ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,CaminhoDaImagem,PrecoUnitario,QuantidadeEmEstoque,CategoriaDeProdutoId,VendedorId")] Produto produto)
+        {
+            if (_context.Produtos == null)
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            if (id != produto.Id)
+                return BadRequest();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(produto);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProdutoExists(produto.Id))
+                        return NotFound();
+                    else
+                        throw;
+                }
+
+                TempData["Sucesso"] = "Produto editado com sucesso.";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CategoriaDeProdutoId"] = new SelectList(_context.CategoriasDeProduto, "Id", "Descricao", produto.CategoriaDeProdutoId);
+            //ViewData["VendedorId"] = new SelectList(_context.Vendedores, "Id", "Id", produto.VendedorId);
+
+            return View(produto);
+        }
+
+        [Route("excluir/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_context.Produtos == null)
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            var produto = await _context.Produtos.Include(p => p.CategoriaDeProduto)
+                                                 .Include(p => p.Vendedor)
+                                                 .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (produto == null)
+                return NotFound();
+
+            return View(produto);
+        }
+
+        [HttpPost("excluir/{id:int}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Produtos == null)
+                return Problem(MensagemAcessoNaoConfigurado);
+
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto != null)
+                _context.Produtos.Remove(produto);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.Produtos.Any(e => e.Id == id);
+        }
+
+        private async Task<string?> ObterUsuarioIdAsync()
+        {
+            IdentityUser? user = await _userManager.GetUserAsync(HttpContext.User);
+            return user?.Id.ToString();
+        }
+
+    }
+}
