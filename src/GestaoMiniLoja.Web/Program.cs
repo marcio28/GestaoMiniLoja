10351f4ff6b9b8ac797e283efa925f5effb4bb79
Márcio Gomes Gonçalves <gomes@metha.com.br>
2025-04-02 18:00:08 -0300

    Configuração da conexão com o banco de dados ajustada para ser resiliente.

--- src/GestaoMiniLoja.Web/Program.cs
+++ src/GestaoMiniLoja.Web/Program.cs
@@ -7,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, a => a.EnableRetryOnFailure(
+                                                    maxRetryCount: 2,
+                                                    maxRetryDelay: TimeSpan.FromSeconds(5),
+                                                    errorNumbersToAdd: null)));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
